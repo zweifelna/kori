c5e3f9809f3c1629a6598c49bdc4566aa71c4595
Nathan Zweifel <nathan.zweifel@heig-vd.ch>
2022-07-21 17:01:01 +0200

    add different recognition for genders

--- src/examples/voice/story1.py
+++ src/examples/voice/story1.py
@@ -44,6 +44,17 @@ def main():
     parser.add_argument('--language', default=locale_language())
     args = parser.parse_args()
     step = 0
+    check = 0
+    characterName = ''
+    characterGender = ''
+    characterPronom = ''
+    villageName = ''
+    locality = ''
+    biome = ''
+    ogreName = ''
+    ogreAdjective = ''
+
+
 
     logging.info('Initializing for language %s...', args.language)
     hints = get_hints(args.language)
@@ -53,7 +64,6 @@ def main():
 
 
     with Board() as board:
-        check = 0
         while True:
             board.led.state = Led.ON
             board.button.wait_for_press()
@@ -68,30 +78,88 @@ def main():
                 
                 text = text.lower()
                 if 'oui' in text and step == 0:
-                    aiy.voice.tts.say('Il était une fois dans un royaume lointain...')
-                    aiy.voice.tts.say("Comment s'appelle le héro ?")
+                    aiy.voice.tts.say("Nous allons créer l'histoire ensemble.")
+                    aiy.voice.tts.say("Pour commencer, comment se nomme le personnage principal ?")
                     step += 1
                     break
                 elif step == 1:
-                    aiy.voice.tts.say("Le héro s'appelait "+text)
-                    aiy.voice.tts.say("Comment s'appelle son royaume ?")
+                    characterName = text
+                    aiy.voice.tts.say("Le personnage s'appelle"+characterName)
+                    aiy.voice.tts.say("Est-ce un garçon ou une fille ?")
                     step += 1
                     break
                 elif step == 2:
-                    aiy.voice.tts.say("Le royaume s'appelait "+text)
-                    aiy.voice.tts.say("Comment s'appelle son roi ?")
+                    while characterGender != 'garçon' and characterGender != 'un garçon' and characterGender != 'fille' and characterGender != 'une fille':
+                        # characterGender = text.replace('un', '', 1)
+                        # characterGender = text.replace('une', '', 1)
+                        characterGender = text
+                        if characterGender == 'garçon' or characterGender == 'un garçon':
+                            characterPronom = 'il'
+                            aiy.voice.tts.say("Le personnage est un garçon")
+                            step += 1
+                            break
+                        elif characterGender == 'fille' or characterGender == 'une fille':
+                            characterPronom = 'elle'
+                            aiy.voice.tts.say("Le personnage est une fille")
+                            step += 1
+                            break
+
+                        aiy.voice.tts.say("Je n'ai pas compris, est-ce un garçon ou une fille ?")
+                        break
+                        
+                    aiy.voice.tts.say("Comment s'appelle le village ?")
                     step += 1
                     break
                 elif step == 3:
-                    aiy.voice.tts.say("Le roi s'appelait "+text)
-                    aiy.voice.tts.say("Comment s'appelle son chat ?")
+                    villageName = text
+                    aiy.voice.tts.say("Le village s'appelle "+villageName)
+                    aiy.voice.tts.say("Le village se trouve vers un désert, une forêt ou un lac ?")
                     step += 1
                     break
                 elif step == 4:
-                    aiy.voice.tts.say("Le chat s'appelait "+text)
-                    aiy.voice.tts.say("Comment s'appelle son chien ?")
+                    biome = text
+                    aiy.voice.tts.say("Le biome est "+text)
+                    aiy.voice.tts.say("Comment s'appelle l'ogre ?")
                     step += 1
                     break
+                elif step == 5:
+                    ogreName = text
+                    aiy.voice.tts.say("L'ogre s'appelle "+ogreName)
+                    aiy.voice.tts.say("Donne moi un adjectif qui décrit l'ogre".)
+                    step += 1
+                    break
+                elif step == 6:
+                    ogreAdjective = text
+                    aiy.voice.tts.say("L'ogre est "+ogreAdjective)
+                    aiy.voice.tts.say("Comment s'appelle le pays ou se déroule l'histoire ?")
+                    step += 1
+                    break
+                elif step == 7:
+                    locality = text
+                    aiy.voice.tts.say("Le pays s'appelle "+locality)
+                    aiy.voice.tts.say("Merci pour ces informations, je vais créer l'histoire.")
+                    aiy.voice.tts.say("Voulez-vous l'entendre ?")
+                    step += 1
+                    break
+                elif step == 8:
+                    if 'oui' in text:
+                        aiy.voice.tts.say("Très bien.")
+                        aiy.voice.tts.say("Il était une fois "+characterName)
+                        if characterGender == 'garçon':
+                            aiy.voice.tts.say("un petit "+characterGender+" plein de vie" )
+                        elif characterGender == 'fille':
+                            aiy.voice.tts.say("une petite "+characterGender+" pleine de vie.")
+                        aiy.voice.tts.say(" "+characterPronom+" habitait dans le village de "+villageName+" dans le biome "+biome+" en "+locality+". Chaque jour, "+characterPronom+" voyait le soleil apparaître derrière la colline et "+characterPronom+" demandait à sa mère : Maman, qu'y a-t-il derrière cette colline ?" +". Derrière la colline, il y un biome "+biome+", un endroit qui n'est pas fait pour les enfants. Seuls les adultes peuvent s'y aventurer car c'est un monde dangereux pouur qui ne connaît pas ses secrets. Un jour, tu pourras toi aussi aller au-delà de la colline. Mais avant, il te faut grandir et écouter les anciens : ils ont beaucoup de choses à t'apprendre.")
+                        aiy.voice.tts.say("Mais "+characterName+" n’écoutait jamais personne. "+characterPronom+" préférait jouer avec ses amis et n’en faire qu’à sa tête. Un jour, "+characterPronom+" s’aventura en dehors du village mais sa mère rattrapa son enfant et le ramena fermement par le bras en lui disant : Ne t’éloigne jamais plus, c’est beaucoup trop dangereux. Je vais te dire ce qu’il y a derrière la colline : il y a "+ogreName+", l’ogre "+ogreAdjective+" qui dévore les enfants perdus.")
+                        
+                    
+
+                        step += 1
+                        break
+                    elif 'non' in text:
+                        aiy.voice.tts.say("Je vais créer l'histoire pour vous et vous pourrez l'écouter plus tard.")
+                        step = 0
+                        break
 
                 
                 
